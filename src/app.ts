f3c81ad9615b1f1cb4b7c85a23b6c37e2e632386
Frank Aka <fakaman47@gmail.com>
2026-09-23 09:46:29 +0100

    Middleware Function

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,11 @@ const PORT = process.env.PORT || 3000;
 
 const app: Application = express();
 
+app.use((req, _res, next) => {  
+    console.log(`${req.method} ${req.originalUrl}`);
+    next();
+});
+
 app.get("/ping", async (_req : Request, res: Response) => {
     res.json({
     message: "hello from Frank's server"
@@ -18,3 +23,5 @@ app.get("/pineapple", async (_req : Request, res: Response) => {
 app.listen(PORT, () => {
     console.log("Server is running on port", PORT);
     });
+
+
