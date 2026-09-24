bdbc50cfba7735ff470495468a56ce9343ddb9e1
Frank <fakaman47@gmail.com>
2026-09-24 17:06:32 +0100

    Task 4

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import carRoutes from './routes/cars';
 import { env } from "./config/env";
 import { connectDB } from "./config/database";
 import {authenticateKey} from './middleware/auth.middleware';
-
+import { routeLogging } from './middleware/route.middleware';
 
 
 const port = env.port;
@@ -13,8 +13,7 @@ app.use('/api/v1/cars',authenticateKey, carRoutes);
 app.use(express.json());
 
 app.use((req, _res, next) => {  
-    console.log(`${req.method} ${req.originalUrl}`);
-    next();
+    routeLogging(req, _res, next);
 });
 
 app.get("/ping", async (_req : Request, res: Response) => {
---
+++ src/middleware/route.middleware.ts
@@ -0,0 +1,6 @@
+import { Request, Response, NextFunction } from 'express';
+
+export const routeLogging = (req: Request, res: Response, next: NextFunction): void => {
+    console.log(`${req.method} ${req.originalUrl}`);
+    next();
+};
